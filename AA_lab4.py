590b3e0b168b991f895b7fbe049797160fb52df9
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2022-05-30 13:41:00 +0200

    podpunkt b (symulator ataku) done :)

--- AA_lab4.py
+++ AA_lab4.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 q = 0.2
 p = 1-q
@@ -22,9 +23,45 @@ for q in np.arange(0.02, 0.5, 0.02):
     PNakamoto = 1 - sumnak
     P = (q/p)**(n-k)
     PGrunspan = 1 - sumgrun
-    plt.scatter(q,PNakamoto, color='k', marker='.')
+    plt.plot(q,PNakamoto, color='b', marker='.', label='Nakamoto' if q == 0.02 else "")
+    plt.plot(q,PGrunspan, color='g', marker='.', label='Grunspan' if q == 0.02 else "")
+    plt.plot(q,P, color='orange', marker='.', label='P' if q == 0.02 else "")
 plt.xlabel('q')
 plt.ylabel('P')
 plt.title('P')
+
+plt.legend()
 plt.show()
+
+
+########## podpunkt b ###########
+
+def symulator_ataku(n,q):
+    adw_counter=0
+    ucz_counter=0 # liczba wykopanych blokow przez adwersarza i uczwiwe wezly
+    while ucz_counter < n:
+        x = random.random()
+        if x <= q:
+            adw_counter += 1
+        else:
+            ucz_counter += 1
+    while ucz_counter - adw_counter < 100:
+        if adw_counter >= ucz_counter:
+            return 1 # atak sie powiodl
+        x = random.random()
+        if x <= q:
+            adw_counter += 1
+        else:
+            ucz_counter += 1
+    return 0 # atak sie nie powiodl
+
+counter=0
+n = 6
+q = 0.3
+for i in range(0,10000):
+    counter += symulator_ataku(n,q)
+P_ataku = counter/100
+print('n = ',n,'\nq = ',q,'\nPrawdopodobienstwo skutecznego ataku: ',P_ataku,'%.',sep='')
+
+# warto tutaj zrobic wykres P ataku w zaleznosci od q przy ustalonych n (kilka wykresow); to proste :)
 
